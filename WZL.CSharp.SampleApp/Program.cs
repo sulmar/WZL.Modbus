16e009bf9ba175df7246bc05a5aa57d2d6168f78
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-05-08 10:10:16 +0200

    Utworzenie podstawowej aplikacji

--- WZL.CSharp.SampleApp/Program.cs
+++ WZL.CSharp.SampleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WZL.CSharp.Models;
 
 namespace WZL.CSharp.SampleApp
 {
@@ -10,6 +11,16 @@ namespace WZL.CSharp.SampleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Podaj imię: ");
+
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("Witaj " + firstname);
+
+            Device device = new Device();
         }
     }
+
 }
