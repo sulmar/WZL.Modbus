68e2e36a87bd3fe733a0ca4de75691b1e8f2a7c4
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-05-08 13:09:15 +0200

    Utworzenie przykładowej klasy, przeciążanie metod

---
+++ WZL.CSharp.SampleApp/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WZL.CSharp.SampleApp
+{
+    internal class Calculator
+    {
+        
+
+
+        private void Init()
+        {
+
+        }
+
+        private void Print(int x)
+        {
+            Console.WriteLine(x);
+        }
+
+
+        // Przeciążanie metod
+        public int Add(int x, int y)
+        {
+            Init();
+
+            int result = x + y;
+
+            return result;
+        }
+
+        public int Add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+
+        // TODO: Dodaj metodę do mnożenia 2 liczb całkowitych o nazwie Multiply
+
+
+        // Parametr opcjonalny
+        public int Multiply(int x, int y, int z = 1)
+        {
+            return x * y * z;
+        }
+
+        // C# 7.0
+
+        //public (int i , int j) Extract(int x, int y, int z)
+        //{
+        //    return new { i = 10, j = 2 };
+        //}
+    }
+}
