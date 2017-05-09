79fd94786360617e06224e1f09f0f8087544e757
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-05-09 12:47:41 +0200

    Odczytanie napięcia stałego

--- WZL.Modbus.ConsoleClient/Program.cs
+++ WZL.Modbus.ConsoleClient/Program.cs
@@ -14,8 +14,52 @@ namespace WZL.Modbus.ConsoleClient
     {
         static void Main(string[] args)
         {
+            GetVoltageTest();
 
             GetTemperatureTest();
+        }
+
+        private static void GetVoltageTest()
+        {
+            var hostname = ConfigurationManager.AppSettings["hostname"];
+            var port = int.Parse(ConfigurationManager.AppSettings["port"]);
+            var slaveId = byte.Parse(ConfigurationManager.AppSettings["N30H_VoltDC"]);
+
+            ushort startAddress = 7010;
+            ushort numRegisters = 2;
+
+            Console.WriteLine($"Connecting to {hostname}:{port}");
+
+            // Określa zakres działania obiektu i utomatycznie wywołanie metodę Dispose
+            // UWAGA: using można użyć tylko dla obiektów, które implementują interfejs IDisposable
+
+            try
+            {
+                using (var client = new TcpClient(hostname, port))
+                using (var master = ModbusIpMaster.CreateIp(client))
+                {
+                    Console.WriteLine("Connected.");
+
+                    Console.WriteLine("Press any key to exit.");
+
+                    while (!Console.KeyAvailable)
+                    {
+                        ushort[] inputs = master.ReadInputRegisters(slaveId, startAddress, numRegisters);
+
+                        float voltage = Converter.ConvertToFloat(inputs[0], inputs[1]);
+
+                        Console.WriteLine($"Voltage: {voltage} V");
+
+                        Thread.Sleep(1000);
+                    }
+                }
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Błąd połączenia. Sprawdź okablowanie oraz konfigurację.");
+            }
+
         }
 
         private static void GetTemperatureTest()
