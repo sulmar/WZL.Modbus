575e75dcd8f8b209ace0a23b67d4554e9f5a2051
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-05-11 18:04:33 +0200

    Sterowanie zasilaczem

---
+++ WZL.EAServices/SupplierService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using WZL.Services;
+
+namespace WZL.EAServices
+{
+    public class SupplierService : IVoltageOutputService, ICurrentOutputService, IOutputService
+    {
+        private string hostname;
+        private int port;
+
+
+        public SupplierService()
+        {
+            hostname = ConfigurationManager.AppSettings["supplierHostname"];
+            port = int.Parse(ConfigurationManager.AppSettings["supplierPort"]);
+        }
+
+        void IOutputService.Off()
+        {
+            Send("OUTPUT OFF");
+        }
+
+        void IOutputService.On()
+        {
+            Send("OUTPUT ON");
+        }
+
+        void ICurrentOutputService.Set(float value)
+        {
+            Send($"CURRENT {value.ToString(CultureInfo.InvariantCulture)}");
+        }
+
+        void IVoltageOutputService.Set(float value)
+        {
+            Send($"VOLT {value.ToString(CultureInfo.InvariantCulture)}");
+        }
+
+        private void Send(string command)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(command);
+
+            using (var client = new TcpClient(hostname, port))
+            using (var stream = client.GetStream())
+            {
+                stream.Write(data, 0, data.Length);
+
+                byte[] lf = { (byte)'\n' };
+
+                stream.Write(lf, 0, 1);
+            }
+        }
+    }
+}
---
+++ WZL.Services/IVoltageService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WZL.Services
+{
+
+    public interface IVoltageInputService
+    {
+        float Get();
+    }
+
+    public interface IVoltageOutputService
+    {
+        void Set(float value);
+    }
+
+    public interface ICurrentInputService
+    {
+        float Get();
+
+    }
+
+    public interface ICurrentOutputService
+    {
+        void Set(float value);
+
+        
+    }
+
+    public interface IOutputService
+    {
+        void On();
+
+        void Off();
+    }
+
+    public interface IPowerService
+    {
+        float Get();
+    }
+
+    public interface IBinaryService
+    {
+        bool[] Get();
+
+        void Set(bool[] outputs);
+    }
+}
